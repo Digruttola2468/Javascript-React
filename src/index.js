a0e73e33c219b624076f711b2960cee1094f3d66
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-10 14:23:45 -0300

    arrays

--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //No es necesario agregar las extenciones .js
-import {Posts} from './Posts';
+import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const users = [
+  {
+    id: 1,
+    name: "Ivan",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "John",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Brisa",
+    image: "https://robohash.org/user3",
+  },
+];
 
 //PascalCase
 root.render(
   <>
-    <Posts/>
+    {users.map((user, index) => {
+      return (
+        <div key={index}>
+          <h1>{user.name}</h1>
+          <img src={user.image}/>
+        </div>
+      );
+    })}
   </>
 );
